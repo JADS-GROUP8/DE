22442491ecbb74ab36834f4abe4ccd51d8adeee9
Mikolaj Hilgert <mikolaj.hilgert@protonmail.com>
2024-10-22 14:44:03 +0200

    update notebok

--- prediction-api/heart_disease_predictor.py
+++ prediction-api/heart_disease_predictor.py
@@ -1,36 +1,40 @@
-import json
 import os
 import pickle
-
 import pandas as pd
 from flask import jsonify
 import logging
-from io import StringIO
-
 
 class HeartDiseasePredictor:
     def __init__(self):
+        # Initialize the model attribute to None
         self.model = None
 
     def load_model(self, file_path):
+        # Load the model from the specified file path using pickle
         with open(file_path, 'rb') as file:
             return pickle.load(file)
 
     def predict_single_record(self, prediction_input):
+        # Log the prediction input for debugging purposes
         logging.debug(prediction_input)
+        
+        # Check if the model is already loaded
         if self.model is None:
             try:
+                # Try to get the model repository path from environment variables
                 model_repo = os.environ['MODEL_REPO']
                 file_path = os.path.join(model_repo, "heart_disease_model.pkl")
                 self.model = self.load_model(file_path)
             except KeyError:
+                # If MODEL_REPO is not defined, print a message and load the model from the default path
                 print("MODEL_REPO is undefined")
                 self.model = self.load_model('heart_disease_model.pkl')
-
-        df = pd.read_json(StringIO(json.dumps(prediction_input)), orient='records')
+        
+        # Convert the prediction input to a DataFrame
+        df = pd.DataFrame([prediction_input])
+        
+        # Make a prediction using the loaded model
         y_pred = self.model.predict(df)
-        logging.info(y_pred[0])
-        status = (y_pred[0] > 0.5)
-        logging.info(type(status[0]))
-        # return the prediction outcome as a json message. 200 is HTTP status code 200, indicating successful completion
-        return jsonify({'result': str(status[0])}), 200
+        
+        # Return the prediction outcome as a JSON message with HTTP status code 200
+        return jsonify({'result': str(y_pred[0])}), 200
--- prediction-ui/app.py
+++ prediction-ui/app.py
@@ -8,38 +8,45 @@ from flask import Flask, request, render_template, jsonify
 # Flask constructor
 app = Flask(__name__)
 
-
 # A decorator used to tell the application
 # which URL is associated function
-@app.route('/checkdiabetes', methods=["GET", "POST"])
-def check_diabetes():
+@app.route('/checkheartdisease', methods=["GET", "POST"])
+def check_heart_disease():
     if request.method == "GET":
         return render_template("input_form_page.html")
 
     elif request.method == "POST":
-        prediction_input = [
-            {
-                "ntp": int(request.form.get("ntp")),  # getting input with name = ntp in HTML form
-                "pgc": int(request.form.get("pgc")),  # getting input with name = pgc in HTML form
-                "dbp": int(request.form.get("dbp")),
-                "tsft": int(request.form.get("tsft")),
-                "si": int(request.form.get("si")),
-                "bmi": float(request.form.get("bmi")),
-                "dpf": float(request.form.get("dpf")),
-                "age": int(request.form.get("age"))
+        prediction_input = {
+            "Age": int(request.form.get("age")),
+            "Sex": int(request.form.get("sex")),  # Assuming 'sex' is a dropdown with index values
+            "ChestPainType": int(request.form.get("chestpain")),  # Assuming 'chestpain' is a dropdown with index values
+            "Cholesterol": int(request.form.get("cholesterol")),
+            "FastingBS": int(request.form.get("fastingbs")),
+            "MaxHR": int(request.form.get("maxhr")),
+            "ExerciseAngina": int(request.form.get("exerciseangina")),  # Assuming 'exerciseangina' is a dropdown with index values
+            "Oldpeak": float(request.form.get("oldpeak")),
+            "ST_Slope": int(request.form.get("stslope"))  # Assuming 'stslope' is a dropdown with index values
             }
-        ]
+        
 
-        logging.debug("Prediction input : %s", prediction_input)
+        logging.error("Prediction input : %s", prediction_input)
 
         # use requests library to execute the prediction service API by sending an HTTP POST request
         # use an environment variable to find the value of the diabetes prediction API
         # json.dumps() function will convert a subset of Python objects into a json string.
         # json.loads() method can be used to parse a valid JSON string and convert it into a Python Dictionary.
         predictor_api_url = os.environ['PREDICTOR_API']
-        res = requests.post(predictor_api_url, json=json.loads(json.dumps(prediction_input)))
 
-        prediction_value = res.json()['result']
+        logging.error(prediction_input)
+
+        res = requests.post(predictor_api_url, json=prediction_input)
+
+        logging.error(res)
+
+        if (res.status_code != 200):
+            return jsonify(message="Prediction API failed"), 500
+
+        prediction_value = res.json().get('result')
         logging.info("Prediction Output : %s", prediction_value)
         return render_template("response_page.html",
                                prediction_variable=prediction_value)
@@ -47,7 +54,7 @@ def check_diabetes():
     else:
         return jsonify(message="Method Not Allowed"), 405  # The 405 Method Not Allowed should be used to indicate
     # that our app that does not allow the users to perform any other HTTP method (e.g., PUT and  DELETE) for
-    # '/checkdiabetes' path
+    # '/checkheartdisease' path
 
 
 # The code within this conditional block will only run the python file is executed as a
